b6a1cbae250f726b878c779b229895fec38301c7
light <estarosa@list.ru>
2022-10-13 22:16:28 +0300

    integrate header menu| pages| icons

---
+++ src/components/pages/mainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import Header from '../../header/Header'
+import ReadyDecision from '../../readyDecisions/ReadyDesicions'
+
+function MainPage() {
+    return (
+        <>
+            <Header />
+            <ReadyDecision />
+        </>
+    )
+}
+
+export default MainPage
